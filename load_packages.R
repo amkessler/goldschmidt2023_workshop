f142e1b0e7143a3c2740d6b9583129ccf2727b58
Aaron Kessler <kessler.aaron@gmail.com>
2023-02-26 21:59:59 -0500

    initial commit

---
+++ load_packages.R
@@ -0,0 +1,5 @@
+### run this one time, to install all package needed for the modules
+### used in this workshop
+
+install.packages(c("tidyverse", "lubridate", "janitor", "tidyquant", "writexl", "nycflights13", "readxl", 
+  "RColorBrewer", "plotly", "tigris", "sf", "tmap", "tmaptools", "htmltools", "here"))
